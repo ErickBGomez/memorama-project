3fe5ff1c33b396682273f49129ee983f3b47d604
Erick B Gómez <90425287+ErickBGomez@users.noreply.github.com>
2022-01-17 18:24:41 -0600

    Save random cards from allCards to allCards2
    Cards are selected randomly in allCards and are stored in allCards2, but there is a security pattern to prevent repeated cards to be part of allCards2

--- Memorama Project/Assets/Scripts/Deck of Cards/AutoGeneratePairID.cs
+++ Memorama Project/Assets/Scripts/Deck of Cards/AutoGeneratePairID.cs
@@ -16,20 +16,67 @@ public class AutoGeneratePairID : MonoBehaviour
 
     Transform[] allCards;
 
+
+    bool isCardRepeated;
+    int randomIndex;
+    Transform cardSelected;
+    public List<Transform> allCards2 = new List<Transform>();
+
+
     #endregion
 
     #region Unity Methods
 
     void OnEnable()
     {
+        // Conseguir referencia del mazo
         allCards = GetComponent<CountAllCards>().cards;
 
+        /*
+        // Agregar aleatoriamente el índice de las cartas
         foreach(Transform card in allCards)
         {
             randomPairID = Random.Range(0, allCards.Length / 2);
 
             card.GetComponent<CardInfo>().pairID = randomPairID;
+        }*/
+                
+
+
+        while (allCards2.Count < allCards.Length)
+        {
+            // Iniciamos con false, ya que el bucle se tiene que reiniciar
+            isCardRepeated = false;
+
+            // Escoger un índice aleatorio en allCards 
+            randomIndex = Random.Range(0, allCards.Length);
+
+            // Tomar esa carta
+            cardSelected = allCards[randomIndex];
+
+            // Verificar si no existe en el mazo
+            foreach (Transform card in allCards2)
+            {
+                // Si la carta existe en allCards2, terminar el bucle foreach
+                if (cardSelected == card)
+                {
+                    isCardRepeated = true;
+
+                    break;
+                }
+            }
+
+            // Si no existe: Agregar carta
+            if (!isCardRepeated)
+                allCards2.Add(cardSelected);
+
         }
+        
+
+        // Establecer el número del PairID (dos veces el mismo valor, luego, aumentar ++)
+
+
+
     }
 
     #endregion
