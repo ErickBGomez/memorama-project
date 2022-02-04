28c0bfe73e4bebcf5f09a863018119bb254fe3d2
Erick B Gómez <90425287+ErickBGomez@users.noreply.github.com>
2022-02-03 22:42:36 -0600

    Initializing Game Modes

---
+++ Memorama Project/Assets/Scripts/UI/Game Modes/GameModes.cs
@@ -0,0 +1,73 @@
+/*
+*********************************
+ Script made by: Erick B. Gomez
+ https://github.com/ErickBGomez
+*********************************
+*/
+
+using UnityEngine;
+
+public class GameModes : MonoBehaviour
+{
+    #region Events
+
+
+
+    #endregion
+
+    #region Variables
+
+
+
+    #endregion
+
+    #region Unity Methods
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    #endregion
+
+    #region Public Methods
+
+
+
+    #endregion
+
+    #region Private Methods
+
+
+
+    #endregion
+
+    #region Subscribe and Unsubscribe Events
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    #endregion
+}
